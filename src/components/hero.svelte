ed790919fb34de7c9e04f225662b32b4309ff163
Joshua Weaver <jweaver911@gmail.com>
2022-03-24 21:24:50 -0400

    changes hero text

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -14,19 +14,19 @@
 			>
 				<div class="sm:text-center lg:text-left">
 					<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-						<span class="block text-rose-600 xl:inline">&lt;coders&gt;</span>
+						<span class="block text-rose-600 xl:inline">&lt;coders/&gt;</span>
 						<span class="block text-slate-500 xl:inline">Liturgy</span>
 					</h1>
 					<p
 						class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
 					>
-						Prayerful heart redirections for Christian coders.
+						Prayerful heart meditations for Christian coders.
 					</p>
 					<div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
 						<div class="rounded-md shadow">
 							<a
 								href="/prayers"
-								class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base 
+								class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base
                                 font-medium rounded-md text-slate-800 bg-yellow-400 hover:bg-yellow-500 md:py-4 md:text-lg md:px-10"
 							>
 								Browse by Topic
