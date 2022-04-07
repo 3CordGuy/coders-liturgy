bae331dd91ff47beed835389cee0574c7cc10eeb
Joshua Weaver <jweaver911@gmail.com>
2022-04-06 23:02:53 -0400

    adds some prose styling

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,4 +2,6 @@
 	<title>Coder's Liturgy | About</title>
 </svelte:head>
 
-<h1>Write a little history/about page!</h1>
+<section class="prose lg:prose-lg prose-slate dark:prose-invert">
+	<h1 class="text-rose-600">Write a little history/about page!</h1>
+</section>
