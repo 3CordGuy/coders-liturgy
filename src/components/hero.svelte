f9d1c4dd7e6b5b00a1c2001668f43455b0c2ea3e
Joshua Weaver <jweaver911@gmail.com>
2022-04-06 23:01:51 -0400

    remove unused markup

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -1,17 +1,10 @@
 <script>
 </script>
 
-<div class="relative bg-white dark:bg-slate-900 overflow-hidden">
+<div class="relative bg-white dark:bg-slate-900">
 	<div class="max-w-7xl mx-auto">
 		<div class="relative pt-6 px-4 sm:px-6 lg:px-8">
 			<slot name="nav" />
 		</div>
-	</div>
-	<div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-		<!-- <img
-			class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-			src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-			alt=""
-		/> -->
 	</div>
 </div>
