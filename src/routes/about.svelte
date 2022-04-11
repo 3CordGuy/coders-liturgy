410b2c77646de5803ef4fec6d363b00d0b1ef298
Joshua Weaver <jweaver911@gmail.com>
2022-04-11 12:44:44 -0400

    adds about content

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,5 +3,31 @@
 </svelte:head>
 
 <section class="prose lg:prose-lg prose-slate dark:prose-invert">
-	<h1 class="text-rose-600">Write a little history/about page!</h1>
+	<h1 class="text-rose-600" id="about-page">About Coder's Liturgy</h1>
+	<p>
+		Software Development is not easy. If we believe that the power to do this alone is &quot;within
+		us,&quot; we deceive ourselves. We need others. We need God&#39;s hand guiding us.
+	</p>
+	<p>
+		&quot;If any of you lacks wisdom, let him ask God, who gives generously to all without reproach,
+		and it will be given him” (James 1:5).
+	</p>
+	<p>
+		Much work is good at best, and morally neutral on average. Some is born of proper motives and
+		yet other works get created with great pride or even laziness. Our motives and heart behind our
+		work is where the difference lay in most circumstances. This website aims to provide some simple
+		prayers as a means of heart redirecting, aligning our thoughts to God&#39;s word and what is
+		true at the end of hitting commit and submit on our code.
+	</p>
+	<p>
+		Truthfully, I built this for myself. To assuage the fears, to subdue the grumbling, and to point
+		my heart in a better direction when its muscles of selfishness and pride get flexed, these words
+		are reminders written for myself. They are joined with what I believe are true words from a
+		God-breathed scripture.
+	</p>
+	<p>
+		It&#39;s a bit of a scary thing to bear your heart. But I believe I&#39;m not alone in this. I
+		felt compelled to make this public. Maybe it will help you, reader.
+	</p>
+	<p>— Josh</p>
 </section>
