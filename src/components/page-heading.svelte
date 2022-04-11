389882cb4c9ecedaa5616406ab84f7455e6839dc
Joshua Weaver <jweaver911@gmail.com>
2022-04-11 18:49:54 -0400

    adds props to page heading

--- src/components/page-heading.svelte
+++ src/components/page-heading.svelte
@@ -1,7 +1,16 @@
 <script>
 	export let text = '';
+	export let hasBrackets = true;
 </script>
 
-<h1 class="text-2xl tracking-tight font-extrabold text-rose-600 sm:text-5xl md:text-5xl lowercase">
-	&lt;{text}/&gt;
+<h1
+	class={`text-2xl tracking-tight font-extrabold text-rose-600 sm:text-5xl md:text-5xl ${
+		hasBrackets ? 'lowercase' : ''
+	}`}
+>
+	{#if hasBrackets}
+		&lt;{text}/&gt;
+	{:else}
+		{text}
+	{/if}
 </h1>
