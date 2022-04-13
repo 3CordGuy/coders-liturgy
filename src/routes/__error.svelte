96d312fcdffb354f05ee9fe0ec954019545f1a45
Joshua Weaver <jweaver911@gmail.com>
2022-04-13 08:56:03 -0400

    adds generic 404

---
+++ src/routes/__error.svelte
@@ -0,0 +1,16 @@
+<article class="h-48 prose prose-slate dark:prose-invert">
+	<h1>404 - Whoops!</h1>
+	<h3>That page doesn't exist.</h3>
+	<p>
+		Maybe go back to the <a href="https://codersliturgy.com/prayers/">prayers</a> list instead?
+	</p>
+</article>
+
+<style>
+	h1 {
+		margin-bottom: 1em;
+	}
+	h3 {
+		margin-bottom: 1em;
+	}
+</style>
