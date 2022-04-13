f5d192522d9ea001dc049d483bdf052ee86c11a9
Joshua Weaver <jweaver911@gmail.com>
2022-04-12 20:41:31 -0400

    adds dynamic icon component

---
+++ src/components/icon.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let iconName = 'eye';
+	export let classes = '';
+
+	let icon;
+
+	onMount(async () => {
+		icon = (await import(`../icons/${iconName}.svelte`)).default;
+	});
+</script>
+
+<svelte:component this={icon} {classes} />
