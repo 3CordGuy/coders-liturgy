59b548579daeca712b13ef49c7199293b7f9035b
Joshua Weaver <jweaver911@gmail.com>
2022-04-16 09:22:36 -0400

    improves footer seo

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -8,7 +8,24 @@
 	class="z-10 w-full p-12 h-32 text-slate-400 dark:text-slate-300
     dark:bg-slate-800 bg-slate-100 flex flex-col basis-full"
 >
-	<div class="flex row space-x-8 items-start">
+	<div class="flex flex-col md:flex-row space-y-8 md:space-x-8 items-start">
+		<div class="md:border-r border-slate-400 md:pr-8">
+			<ul class="text-sm space-y-2">
+				<li><a href="/prayers">Prayers for Coders</a></li>
+				<li><a href="/about">About Coder's Liturgy</a></li>
+				<li class="flex row items-center">
+					<a class="text-sm mt-1" href="https://twitter.com/3cordguy">
+						<Twitter classes="mr-2" /></a
+					>
+					<a class="text-sm mt-1" href="https://github.com/3cordguy/coders-liturgy"
+						><Github classes="mr-2" /></a
+					>
+					<a class="text-sm mt-1" href="https://codersliturgy.com/feed/rss.xml"
+						><Rss classes="mr-2" /></a
+					>
+				</li>
+			</ul>
+		</div>
 		<div class="space-y-4">
 			<div class="flex-auto justify-center max-w-7xl text-sm">
 				Copyright ® {new Date().getFullYear()} Josh Weaver. All rights reserved.
@@ -17,13 +34,6 @@
 				Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®),
 				copyright © 2001 by Crossway, a publishing ministry of Good News Publishers.<br />Used by
 				permission. All rights reserved.
-			</div>
-			<div class="flex row items-center">
-				<a class="text-sm" href="https://twitter.com/3cordguy"> <Twitter classes="mr-2" /></a>
-				<a class="text-sm" href="https://github.com/3cordguy/coders-liturgy"
-					><Github classes="mr-2" /></a
-				>
-				<a class="text-sm" href="https://codersliturgy.com/feed/rss.xml"><Rss classes="mr-2" /></a>
 			</div>
 		</div>
 	</div>
