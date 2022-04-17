ce8dd12a623a4d01246ca9e03b5fa4a070382be1
Joshua Weaver <jweaver911@gmail.com>
2022-04-16 21:36:23 -0400

    removes headers from verse api response

--- src/routes/api/verse/[verse].js
+++ src/routes/api/verse/[verse].js
@@ -1,6 +1,6 @@
 export const get = async ({ params }) => {
 	const res = await fetch(
-		`https://api.esv.org/v3/passage/html/?q=${params.verse}&include-footnotes=false&include-subheadings=false&include-verse-anchors=true`,
+		`https://api.esv.org/v3/passage/html/?q=${params.verse}&include-footnotes=false&include-headings=false&include-subheadings=false&include-verse-anchors=true&include-short-copyright=true`,
 		{
 			headers: {
 				Authorization: `Token ${import.meta.env.VITE_ESV_API}`
