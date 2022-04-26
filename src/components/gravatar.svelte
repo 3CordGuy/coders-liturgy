ca61caa9e4cc34bd01e04ffcdb4397baaa8760bf
Joshua Weaver <jweaver911@gmail.com>
2022-04-25 20:59:46 -0400

    updates avatar

--- src/components/gravatar.svelte
+++ src/components/gravatar.svelte
@@ -4,12 +4,19 @@
 </script>
 
 <div class="flex items-center">
-	<div>
-		<img class="inline-block h-14 w-14 rounded-full" src={gravUrl} alt="Gravatar for Josh" />
+	<div class="flex-shrink-0">
+		<img class="inline-block h-24 w-24 rounded-full" src={gravUrl} alt="Gravatar for Josh" />
 	</div>
 	<div class="ml-3">
-		<p class="text-md font-medium dark:text-gray-400 text-gray-400 group-hover:text-gray-900">
+		<div class="text-md font-medium dark:text-gray-400 text-gray-400 group-hover:text-gray-900">
 			{name}
-		</p>
+		</div>
+		<div class="text-sm font-small dark:text-gray-500 text-gray-500">
+			Currently works full time as a Software Engineer for Tithe.ly and has spent the last 10 years
+			as a developer in the Church Management and Non-profit software space. A husband, and father
+			of 3, he helps lead worship for <a class="text-yellow-400" href="https://youareloved.church"
+				>Wawasee Bible Church</a
+			>.
+		</div>
 	</div>
 </div>
