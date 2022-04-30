f19ce037f3d9d0090b91c85ef7f6910d72992d41
Joshua Weaver <jweaver911@gmail.com>
2022-04-30 11:20:27 -0400

    improves responsive text

--- src/components/prayer-list.svelte
+++ src/components/prayer-list.svelte
@@ -11,46 +11,48 @@
 			<li transition:fade={{ delay: 150, duration: 200 }}>
 				<div
 					class="relative focus-within:ring-2
-						   focus-within:p-4 focus-within:ring-yellow-500
-						   flex flex-row grow p-4 items-center bg-gray-100 dark:bg-slate-800 rounded
-						   transition ease-in-out delay-150 hover:bg-slate-200 hover:dark:bg-slate-700
+						   focus-within:ring-yellow-500
+						   flex flex-row grow items-center bg-gray-100 dark:bg-slate-800 rounded
+						   transition ease-in-out delay-150 hover:bg-slate-100 hover:dark:bg-slate-700
 						   hover:rounded hover:shadow-lg duration-300"
 				>
-					<div class="flex flex-row">
-						<div class="mr-4">
+					<div class="flex flex-row overflow-hidden rounded">
+						<div class="grow hover:bg-yellow-500 w-24 h-24">
 							{#if item.meta.icons && item.meta.icons[0]}
 								<Icon
 									iconName={item.meta.icons[0].name}
-									classes={`w-12 h-12 mt-1 text-${item.meta.icons[0].color}`}
+									classes={`w-24 h-24 text-${item.meta.icons[0].color}`}
 								/>
 							{:else}
 								<Icon iconName="heart" classes="w-12 h-12 mt-1 text-yellow-400" />
 							{/if}
 						</div>
 
-						<div>
-							<h3 class="text-sm font-semibold text-slate-500">
-								{#if item.path}
-									<a
-										href={item.path}
-										class="text-xl
-											   transition ease-in-out delay-100
-											   hover:text-yellow-500 dark:hover:text-yellow-400
-											   duration-300
-											   focus:outline-none"
-									>
-										<!-- Extend touch target to entire panel -->
+						<div class="flex px-4 py-2 items-center">
+							<div>
+								<h3 class="text-sm font-semibold text-slate-500">
+									{#if item.path}
+										<a
+											href={item.path}
+											class="text-md md:text-lg lg:text-xl
+												   transition ease-in-out delay-100
+												   hover:text-yellow-500 dark:hover:text-yellow-400
+												   duration-300
+												   focus:outline-none"
+										>
+											<!-- Extend touch target to entire panel -->
+											<span class="absolute inset-0" aria-hidden="true" />
+											{item.meta.title}
+										</a>
+									{:else}
 										<span class="absolute inset-0" aria-hidden="true" />
-										{item.meta.title}
-									</a>
-								{:else}
-									<span class="absolute inset-0" aria-hidden="true" />
-									{item.meta.title} <sup class="uppercase italic">coming soon</sup>
-								{/if}
-							</h3>
-							<p class="mt-1 mb-0 text-sm dark:text-slate-500 text-sm line-clamp-2">
-								{item.meta.description}
-							</p>
+										{item.meta.title} <sup class="uppercase italic">coming soon</sup>
+									{/if}
+								</h3>
+								<p class="mt-1 mb-0 text-xs dark:text-slate-500 md:text-sm line-clamp-2">
+									{item.meta.description}
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
